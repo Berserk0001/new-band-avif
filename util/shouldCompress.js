ec20f40fb76abdebe5dab037666d2e3f60225c84
denYOUvo <59727582+denYOUvo@users.noreply.github.com>
2022-11-27 23:53:39 +0700

    Update shouldCompress.js
    remove minimum conditional threshold

--- util/shouldCompress.js
+++ util/shouldCompress.js
@@ -1,18 +1,18 @@
-const MIN_COMPRESS_LENGTH = 2048; // ~2kb
-const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 10; // ~20kb
+//const MIN_COMPRESS_LENGTH = 2048; // ~2kb
+//const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 10; // ~20kb
 
-function shouldCompress(originType, originSize, webp) {
-//function shouldCompress(originType, originSize) {
+//function shouldCompress(originType, originSize, webp) {
+function shouldCompress(originType, originSize) {
 	if (!originType.startsWith("image")) return false;
 	if (originSize === 0) return false;
-	if (webp && originSize < MIN_COMPRESS_LENGTH) return false;
-	if (	// if png or gif image, and size is less than compression limit (also shouldn't be webp)
-		!webp &&
-		(originType.endsWith("png") || originType.endsWith("gif")) &&
-		originSize < MIN_TRANSPARENT_COMPRESS_LENGTH
-	) {
-		return false;
-	}
+	//if (webp && originSize < MIN_COMPRESS_LENGTH) return false;
+	//if (	// if png or gif image, and size is less than compression limit (also shouldn't be webp)
+	//	!webp &&
+	//	(originType.endsWith("png") || originType.endsWith("gif")) &&
+	//	originSize < MIN_TRANSPARENT_COMPRESS_LENGTH
+	//) {
+	//	return false;
+	//}
 
 	return true;
 }
